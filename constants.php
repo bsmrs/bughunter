4cc3c37ac18be11a5c8d91407b173fe254a16d45
Bruno Miranda <brunodes.miranda@gmail.com>
2015-10-06 17:31:38 -0300

    modified:   constants.php modified:   schema/config.sql new file:   src/Gearman/Entity/WorkerCfg.php
    - Constants to access database was created;
    - Entity to store workers configuration;
    
    ticket #14

--- constants.php
+++ constants.php
@@ -2,3 +2,7 @@
 
 define('DS', DIRECTORY_SEPARATOR);
 define('WORKERS_DIR', 'workers');
+define('DBUSER', 'root');
+define('DBPASS', '');
+define('DBDRIVER', 'pdo_mysql');
+define('DBHOST', '127.0.0.1');
---
+++ src/Gearman/Entity/WorkerCfg.php
@@ -0,0 +1,146 @@
+<?php
+
+namespace Hunter\Gearman\Entity;
+use Respect\Validation\Validator;
+
+/**
+ * This class is a representation of Gearman's worker configuration.
+ * @Entity
+ * @Table(name="gearman_workers", schema="config")
+ */
+class WorkerCfg {
+	/**
+	 * @var Integer internal system ID
+	 * @Id @Column(type="integer", nullable=false) @GeneratedValue
+	 */
+	private $id;
+
+	/**
+	 * @var String $name is the name of worker.
+	 * @Column(type="string", nullable=false, options={"unique": true})
+	 */
+	private $name;
+
+	/**
+	 * @var Integer $min is the minimum number of workers.
+	 * @Column(type="smallint", nullable=false, options={"default": 1, "unsigned": true})
+	 */
+	private $min=1;
+
+	/**
+	 * @var Integer $max is the maximum number of workers.
+	 * @Column(type="smallint", nullable=false, options={"default": 1, "unsigned": true})
+	 */
+	private $max=1;
+
+	/**
+	 * @var Integer $increment is the number of workers that will be incremented by time.
+	 * @Column(type="smallint", nullable=false, options={"default": 1, "unsigned": true})
+	 */
+	private $increment=1;
+
+	/**
+	 * @return Integer id
+	 */
+	public function getId()
+	{
+		return $this->id;
+	}
+
+	/**
+	 * @return name
+	 */
+	public function getName()
+	{
+		return $this->name;
+	}
+
+	/**
+	 * @param String $name
+	 * @return WorkerCfg
+	 */
+	public function setName($name)
+	{
+		Validator::alnum('_')
+			->noWhitespace()
+			->length(1, 255)
+			->check($name);
+
+		$this->name = $name;
+
+		return $this;
+	}
+
+	/**
+	 * @return Integer min
+	 */
+	public function getMin()
+	{
+		return $this->min;
+	}
+
+	/**
+	 * @param Integer $min
+	 * @return WorkerCfg
+	 */
+	public function setMin($min)
+	{
+		Validator::type('int')
+			->int()
+			->between(1, 65535, true)
+			->check($min);
+
+		$this->min = $min;
+
+		return $this;
+	}
+
+	/**
+	 * @return Integer max
+	 */
+	public function getMax()
+	{
+		return $this->max;
+	}
+
+	/**
+	 * @param Integer $max
+	 * @return WorkerCfg
+	 */
+	public function setMax($max)
+	{
+		Validator::type('int')
+			->int()
+			->between(1, 65535, true)
+			->check($max);
+
+		$this->max = $max;
+
+		return $this;
+	}
+
+	/**
+	 * @return Integer increment
+	 */
+	public function getIncrement()
+	{
+		return $this->increment;
+	}
+
+	/**
+	 * @param Integer $increment
+	 * @return WorkerCfg
+	 */
+	public function setIncrement($increment)
+	{
+		Validator::type('int')
+			->int()
+			->between(1, 65535, true)
+			->check($increment);
+
+		$this->increment = $increment;
+
+		return $this;
+	}
+
+}
